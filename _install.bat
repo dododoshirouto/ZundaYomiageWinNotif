9c2314cc9744481bf2275ee5ba15027bcad43b0d
dododoshirouto <yfkf19961009@gmail.com>
2025-03-15 02:35:55 +0900

    インストールスクリプトを更新して、仮想環境の存在チェックやVOICEVOXのインストール状況を表示するようにしたよ。

--- _install.bat
+++ _install.bat
@@ -1,24 +1,39 @@
 @echo off
 
 IF NOT EXIST venv\Scripts\activate (
+    echo create venv.
     python -m venv venv
+) ELSE (
+    echo venv is exists.
 )
 
+echo install python modules.
 "venv\Scripts\python.exe" -m pip install -r requirements.txt
 
 set VOICEVOX_VERSION=0.15.7
+echo VOICEVOX_VERSION is %VOICEVOX_VERSION%.
 
 @REM download and install voicevox_core
 
 IF NOT EXIST voicevox-download-windows-x64.exe (
     echo Downloading download-windows-x64.exe from voicevox_core ...
     curl -L -o voicevox-download-windows-x64.exe https://github.com/VOICEVOX/voicevox_core/releases/download/%VOICEVOX_VERSION%/download-windows-x64.exe
+) ELSE (
+    echo voicevox-download-windows-x64.exe is exists.
 )
 
 IF NOT EXIST voicevox_core.dll (
+    echo install voicevox.
     voicevox-download-windows-x64.exe -v %VOICEVOX_VERSION% -o ./
+) ELSE (
+    echo voicevox is installed.
 )
 
 IF NOT EXIST venv\Lib\site-packages\voicevox_core (
+    echo install voicevox_core modules.
     "venv\Scripts\python.exe" -m pip install https://github.com/VOICEVOX/voicevox_core/releases/download/%VOICEVOX_VERSION%/voicevox_core-%VOICEVOX_VERSION%+cpu-cp38-abi3-win_amd64.whl
+) ELSE (
+    echo voicevox_core modules is installed.
 )
+
+pause
